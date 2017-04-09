b9759d9f9d57b24bd0bddd839dcf61c50ee1a71f
icapa9 <icapa.rescuettp@gmail.com>
2017-04-09 07:01:07 +0300

    Исправлены ретиновые изображения, фоны, js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 var menu = document.querySelector(".menu");
 var menuToggle = document.querySelector(".menu__toggle");
 var popup = document.querySelector(".popup");
-var link = document.querySelector(".link-popup");
+var link = document.querySelector(".modal");
+var cartBuy = document.querySelectorAll(".modal");
+
+menu.classList.remove("menu--nojs");
 
 menuToggle.addEventListener("click", function() {
-
-  menu.classList.remove("menu--nojs");
-
   if (menu.classList.contains("menu--closed")) {
     menu.classList.remove("menu--closed");
     menu.classList.add("menu--opened");
@@ -14,9 +14,18 @@ menuToggle.addEventListener("click", function() {
     menu.classList.add("menu--closed");
     menu.classList.remove("menu--opened");
   }
+
 });
 
 link.addEventListener("click", function() {
   event.preventDefault();
   popup.classList.add("popup--opened");
 });
+for (var i = 0; i < cartBuy.length; i++) {
+    cartBuy[i].addEventListener("click", function(event) {
+    event.preventDefault();
+    if (popup) {
+        popup.classList.add("popup--opened");
+    }
+  });
+}
